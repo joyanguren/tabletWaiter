040faace40cccfc2800b637e07c8fad41e1b6c58
Natch Khongpasuk <natchynaat@gmail.com>
2015-07-01 12:18:37 +0700

    cartService.js

---
+++ tabletWaiter/tabletWaiter/AngularApp/Services/cartService.js
@@ -0,0 +1,32 @@
+﻿tabletWaiter.factory('cartService', function ($http) {
+    var cart = [];
+    $scope.quantity;
+    $scope.price;
+
+
+    var addCart = function (item) {
+        cart.push(item);
+    };
+
+    var getCart = function () {
+        return cart;
+    };
+
+    var clearCart = function () {
+        cart = [];
+    };
+
+    var deleteItem = function (item, index) {
+        cart.splice(index, 1);
+    };
+
+    var service = {
+        addCart: addCart,
+        getCart: getCart,
+        clearCart: clearCart,
+        deleteItem: deleteItem
+    };
+
+    return service;
+
+});
