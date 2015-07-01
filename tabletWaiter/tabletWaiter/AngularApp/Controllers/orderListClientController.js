da9bb66306f44fdccb98070a31f8f48098f414a5
jon_innovup <jon@innovup.cc>
2015-07-01 10:46:04 +0200

    Changes in orderList controller and html

--- tabletWaiter/tabletWaiter/AngularApp/Controllers/orderListClientController.js
+++ tabletWaiter/tabletWaiter/AngularApp/Controllers/orderListClientController.js
@@ -5,41 +5,8 @@
     $scope.categories;
     $scope.loading = true;
 
-    dataService.getCategories().then(function (result) {
-        $scope.categories = result.data;
-    });
-
-    dataService.getShowItems().then(function (result) {
-        $scope.items = result.data;
-        allItems = result.data;
-        $scope.itemsCount = result.data.length;
-
-        $scope.loading = false;
-    });
-
-    var getOrder = function () {
-        alert(cartItem.name);
-    };
-
-    $scope.filterCategory = function (categoryId) {
-        $scope.loading = true;
-        $scope.items = [];
-
-        if (categoryId == -1) {
-            $scope.items = allItems;
-            $scope.itemsCount = $scope.items.length;
-            $scope.loading = false;
-            return;
-        }
-
-        angular.forEach(allItems, function (item) {
-            if (item.Categories && item.Categories.match(categoryId)) {
-                $scope.items.push(item);
-            }
-        });
-
-        $scope.itemsCount = $scope.items.length;
-        $scope.loading = false;
+    $scope.getOrder = function () {
+        alert($scope.numberOfItems);
     };
 
     $scope.addItem = function (name, price) {
