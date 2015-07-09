6b9db2ea0fb74c9fb9a8645bfa7d030c82c37835
jon_innovup <jon@innovup.cc>
2015-07-09 09:05:07 +0200

    change loading before the dataService call

--- tabletWaiter/tabletWaiter/AngularApp/Controllers/callForm.js
+++ tabletWaiter/tabletWaiter/AngularApp/Controllers/callForm.js
@@ -1,9 +1,10 @@
 ﻿tabletWaiter.controller('callFormController', ['$scope', '$rootScope', 'dataService', function ($scope, $rootScope, dataService) {
     $scope.showOverlay = false;
     $scope.simpleAlert = function () {
+        $scope.showOverlay = true;
+
         dataService.sendSimpleAlert(window.sessionStorage.tableNumber)
         .success(function (result) {
-            $scope.showOverlay = true;
             swal("Llamada con éxito", "Enseguida le atenderá uno de nuestros camareros", "success")
             $scope.showOverlay = false;
         })
